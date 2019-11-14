58d4845f48bc8218d1318fe91ae99e7995b7d79a
pavansurampudi <57211236+pavansurampudi@users.noreply.github.com>
2019-11-14 12:01:04 +0530

    lio

--- job.router.js
+++
@@ -1,5 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const jobcntrl=require('../controllers/job.cntrl');
-router.post('/addemployee',jobcntrl.addemplyee);
-module.exports=router;
